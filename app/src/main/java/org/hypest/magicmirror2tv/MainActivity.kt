100ec7104d03cd568da258fc4208b0d6e497bbed
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2020-04-18 22:58:46 +0300

    Reload on touch

--- app/src/main/java/org/hypest/magicmirror2tv/MainActivity.kt
+++ app/src/main/java/org/hypest/magicmirror2tv/MainActivity.kt
@@ -2,6 +2,7 @@ package org.hypest.magicmirror2tv
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
@@ -13,6 +14,10 @@ class MainActivity : Activity() {
         webView.settings.javaScriptEnabled = true
         webView.setInitialScale(100)
         webView.settings.builtInZoomControls = true
+        webView.setOnTouchListener(View.OnTouchListener { _, _ ->
+            webView.reload()
+            return@OnTouchListener true
+        })
         webView.loadUrl("http://192.168.1.60:8100");
     }
 }
